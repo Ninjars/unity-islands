a25044da21ca4149e76de304ba3bc21daccf039a
jez <jeremy+github@jeremystevens.net>
2018-01-14 21:51:56 +0000

    implemented basic voronoi(ish?) mesh generation

---
+++ Assets/WorldGenerator/Triangulator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using TriangleNet.Geometry;
+using UnityEngine;
+using TriangleNet.Voronoi;
+using TriangleNet.Meshing;
+using TriangleNet.Topology.DCEL;
+
+namespace WorldGenerator {
+	
+	public class Triangulator {
+
+		public static VoronoiBase generateVoronoi(List<Vector2> points) {
+			var poly = new Polygon (points.Count());
+			foreach (Vector2 point in points) {
+				poly.Add (new TriangleNet.Geometry.Vertex (point.x, point.y));
+			}
+			TriangleNet.Mesh mesh = (TriangleNet.Mesh) poly.Triangulate (new ConstraintOptions() { ConformingDelaunay = true});
+			return new BoundedVoronoi (mesh);
+		}
+
+		public static void triangulateVoronoi(VoronoiBase voronoi, out List<int> outIndices, out List<Vector3> outVertices) {
+			outIndices = new List<int>();
+            outVertices = new List<Vector3>();
+			foreach (Face face in voronoi.Faces) {
+				int offset = outVertices.Count;
+				HalfEdge[] edges = face.EnumerateEdges().ToArray();
+				edges.Reverse ();
+				for(int i = 1; i < edges.Count()-1; i++) {
+					outIndices.Add(offset);
+					outIndices.Add(offset + i);
+					outIndices.Add(offset + i + 1);
+				}
+				outVertices.AddRange ((from edge in edges select new Vector3 ((float)edge.Origin.X, 0f, (float)edge.Origin.Y)).ToList());
+            }
+		}
+    }
+}
---
+++ Assets/WorldGenerator/WorldGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using TriangleNet.Voronoi;
+
+namespace WorldGenerator {
+    public class WorldGenerator : MonoBehaviour {
+
+		private const float worldSize = 1000;
+		private const int pointCount = 100;
+
+        private void Start() {
+            GameObject gameObj = new GameObject();
+            gameObj.name = "Island";
+            MeshFilter meshFilter = gameObj.AddComponent<MeshFilter>();
+            gameObj.AddComponent<MeshRenderer>();
+
+            Mesh mesh = meshFilter.mesh;
+            generateWorld(mesh);
+
+            gameObj.AddComponent<MeshCollider>().sharedMesh = mesh;
+        }
+
+        private void generateWorld(Mesh mesh) {
+            int seed = 12335;
+            
+            List<Vector2> initialPoints = new List<Vector2>(pointCount);
+			initialPoints.Add(new Vector2(0, 0));
+			initialPoints.Add(new Vector2(worldSize, 0));
+			initialPoints.Add(new Vector2(0, worldSize));
+			initialPoints.Add(new Vector2(worldSize, worldSize));
+            System.Random pointRandom = new System.Random(seed);
+            for (int i = 0; i < pointCount; i++) {
+                initialPoints.Add(new Vector2((float)pointRandom.NextDouble() * worldSize, (float)pointRandom.NextDouble() * worldSize));
+            }
+
+			VoronoiBase voronoi = Triangulator.generateVoronoi (initialPoints);
+
+			assignMeshVertices (voronoi, mesh);
+        }
+
+		private void assignMeshVertices(VoronoiBase voronoi, Mesh mesh) {
+			List<int> indices;
+			List<Vector3> positions;
+			Triangulator.triangulateVoronoi(voronoi, out indices, out positions);
+
+			mesh.Clear();
+			mesh.vertices = positions.ToArray();
+			mesh.triangles = indices.ToArray();
+			mesh.RecalculateNormals();
+			flipMeshNormals (mesh);
+
+			Vector2[] myUVs = new Vector2[positions.Count];
+			for (var i = 0; i < positions.Count; i++) {
+				myUVs[i] = new Vector2(positions[i].x / worldSize, positions[i].y / worldSize);
+			}
+			mesh.uv = myUVs;
+		}
+
+		private void flipMeshNormals(Mesh mesh) {
+			var indices = mesh.triangles;
+			var triangleCount = indices.Length / 3;
+			for (var i = 0; i < triangleCount; i++) {
+				var tmp = indices [i * 3];
+				indices [i * 3] = indices [i * 3 + 1];
+				indices [i * 3 + 1] = tmp;
+			}
+			mesh.triangles = indices;
+			// additionally flip the vertex normals to get the correct lighting
+			var normals = mesh.normals;
+			for (var n = 0; n < normals.Length; n++) {
+				normals [n] = -normals [n];
+			}
+			mesh.normals = normals;
+		}
+    }
+}
