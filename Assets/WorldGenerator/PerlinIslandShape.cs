508b69407a7960ded80fc4248aba1a53cfcf8663
Jeremy Stevens <jeremy.stevens@bglgroup.co.uk>
2018-01-30 17:50:26 +0000

    attempt at modifying PerlinIslandShape

--- Assets/WorldGenerator/PerlinIslandShape.cs
+++ Assets/WorldGenerator/PerlinIslandShape.cs
@@ -7,6 +7,8 @@ namespace WorldGenerator {
 		private float size;
         private float offset;
 
+		private float scale = 100f;
+
         public PerlinIslandShape(int seed, float size) {
 			this.size = size;
             System.Random pointRandom = new System.Random(seed);
@@ -15,12 +17,14 @@ namespace WorldGenerator {
 
 		public bool isInside(float x, float y) {
 			float perlinVal = getPerlin(x, y);
-			double lineLength = Mathf.Sqrt(x * x + y * y);
-			return perlinVal > 0.3 + 0.7 * lineLength * lineLength;
+			float polarX = (x/size - 0.5f) * 2;
+			float polarY = (y/size - 0.5f) * 2;
+			float lineLength = (polarX * polarX + polarY * polarY);
+			return perlinVal > 0.3 + 0.7 * lineLength;
 		}
 
 		private float getPerlin(float x, float y) {
-			return Mathf.Clamp01(Mathf.PerlinNoise(offset + x/size, offset + y/size));
+			return Mathf.Clamp01(Mathf.PerlinNoise(scale * (offset + x/size), scale * (offset + y/size)));
 		}
 	}
 }
