b3b3d55e1c0c4982f475b2d6ec4b5ba1bb3b731d
jez <jeremy+github@jeremystevens.net>
2018-03-25 22:48:35 +0100

    split mesh generation code from WorldGenerator out into WorldGenMesh

---
+++ Assets/WorldGenerator/WorldGenMesh.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WorldGenerator {
+	public class WorldGenMesh {
+		private const int vertexLimit = 63000;
+
+        private World world;
+        private GameObject gameObject;
+		private float verticalScale;
+        private Material material;
+
+        public WorldGenMesh(World world, GameObject islandGameObject, float verticalScale, Material material) {
+			this.world = world;
+			this.gameObject = islandGameObject;
+			this.verticalScale = verticalScale;
+			this.material = material;
+		}
+		
+		public void triangulate() {
+			List<int> indices = new List<int>();
+			List<Vector3> vertices = new List<Vector3>();
+			List<Color> colors = new List<Color>();
+			List<Center> sortedCenters = new List<Center>(world.centers);
+			sortedCenters.Sort((a, b) => {
+				bool xLess = a.coord.x < b.coord.x;
+				return xLess || (xLess && a.coord.y < b.coord.y) ? -1 : 1;
+			});
+			foreach (Center center in sortedCenters) {
+				addTrianglesForCenter(center, indices, vertices, colors);
+				if (vertices.Count > vertexLimit) {
+					addMeshSubObject(gameObject, indices, vertices, colors);
+				}
+			}
+			if (vertices.Count > 0) {
+				addMeshSubObject(gameObject, indices, vertices, colors);
+			}
+		}
+
+        private void addMeshSubObject(GameObject containingObject, List<int> indices, List<Vector3> vertices, List<Color> colors) {
+			GameObject gameObject = new GameObject();
+			gameObject.name = "mesh section";
+			gameObject.transform.SetParent(containingObject.transform);
+			gameObject.transform.position = Vector3.zero;
+
+            MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+            Mesh mesh = meshFilter.mesh;
+            gameObject.AddComponent<MeshRenderer>();
+            gameObject.AddComponent<MeshCollider>().sharedMesh = mesh;
+			gameObject.GetComponent<Renderer>().material = material;
+
+			mesh.vertices = vertices.ToArray();
+			mesh.triangles = indices.ToArray();
+			mesh.colors = colors.ToArray();
+			mesh.RecalculateNormals();
+			Vector2[] myUVs = new Vector2[vertices.Count];
+			for (var i = 0; i < vertices.Count; i++) {
+				myUVs[i] = new Vector2(vertices[i].x / world.size, vertices[i].y /  world.size);
+			}
+			mesh.uv = myUVs;
+			
+			indices.Clear();
+			vertices.Clear();
+			colors.Clear();
+		}
+
+		private void buildPedestal(World world, GameObject islandGameObject) {
+			List<int> indices = new List<int>();
+			List<Vector3> vertices = new List<Vector3>();
+			List<Color> colors = new List<Color>();
+			foreach (Center center in world.centers) {
+				if (center.isOnRim) {
+					// addPedestalForCenter(center, indices, vertices, colors);
+					if (vertices.Count > vertexLimit) {
+						addMeshSubObject(islandGameObject, indices, vertices, colors);
+					}
+				}
+			}
+			if (vertices.Count > 0) {
+				addMeshSubObject(islandGameObject, indices, vertices, colors);
+			}
+		}
+
+        // private void addPedestalForCenter(Center center, List<int> indices, List<Vector3> vertices, List<Color> colors) {
+        //     List<Corner> corners = center.corners;
+        //     int startIndex = findNextRim(corners, -1);
+		// 	if (startIndex < 0) {
+		// 		return;
+		// 	}
+
+		// 	int endIndex = findNextRim(corners, startIndex);
+		// 	if (endIndex < 0) {
+		// 		return;
+		// 	} else {
+		// 		Corner corner1 = corners[startIndex];
+		// 		Vector3 v1 = new Vector3((float)corner1.coord.x, corner1.scaledElevation(verticalScale), (float)corner1.coord.y);
+        //     	Vector3 v2 = new Vector3((float)center.coord.x, center.scaledElevation(verticalScale), (float)center.coord.y);
+		// 		Corner corner2 = corners[endIndex];
+		// 		Vector3 v3 = new Vector3((float)corner2.coord.x, corner2.scaledElevation(verticalScale), (float)corner2.coord.y);
+		// 	}
+        // }
+
+        // private int findNextRim(List<Corner> corners, int startIndex) {
+        //     for (int i = 1; i < corners.Count; i++) {
+		// 		if (corners[(startIndex + i) % corners.Count].isIslandRim) {
+		// 			return i;
+		// 		}
+		// 	}
+		// 	return -1;
+        // }
+
+        private void addTrianglesForCenter(Center center, 
+											List<int> indices, 
+											List<Vector3> vertices,
+											List<Color> colors) {
+            Vector3 vertexCenter = new Vector3((float)center.coord.x, center.scaledElevation(verticalScale), (float)center.coord.y);
+            Color color = getColor(center.terrainType);
+			if (center.isClipped) {
+				return;
+			}
+
+            List<Corner> corners = center.corners;
+            for (int i = 0; i < corners.Count; i++) {
+				Corner corner1 = corners[i];
+				int index2 = i + 1 >= corners.Count ? 0 : i + 1;
+				Corner corner2 = corners[index2];
+
+				if (corner1.isClipped || corner2.isClipped) {
+					continue;
+				} else {
+					Vector3 vertex1 = new Vector3((float)corner1.coord.x, corner1.scaledElevation(verticalScale), (float)corner1.coord.y);
+					Vector3 vertex2 = new Vector3((float)corner2.coord.x, corner2.scaledElevation(verticalScale), (float)corner2.coord.y);
+					addTriangle(vertexCenter, vertex1, vertex2, color, indices, vertices, colors);
+				}
+            }
+		}
+
+		private static void addTriangle(Vector3 a, Vector3 b, Vector3 c, Color color, 
+									List<int> indices, List<Vector3> vertices, List<Color> colors) {
+				int indicesOffset = vertices.Count;
+            	vertices.Add(a);
+            	vertices.Add(b);
+            	vertices.Add(c);
+				colors.Add(color);
+				colors.Add(color);
+				colors.Add(color);
+                indices.Add(indicesOffset);
+                indices.Add(indicesOffset + 2);
+                indices.Add(indicesOffset + 1);
+		}
+
+        private void assignVertexColors(World world, Mesh mesh) {
+			Color[] colors = new Color[mesh.vertices.Length];
+			foreach (Corner corner in world.corners) {
+				foreach (int index in corner.vertexIndices) {
+					colors[index] = getColor(corner.terrainType);
+				}
+			}
+			mesh.colors = colors;
+		}
+		
+		private Color getColor(TerrainType terrainType) {
+			switch (terrainType) {
+				case TerrainType.OCEAN:
+					return Color.blue;
+				case TerrainType.LAKE:
+					return Color.cyan;
+				case TerrainType.LAND:
+					return Color.grey;
+                case TerrainType.COAST:
+                    return Color.yellow;
+                default:
+					return Color.white;
+			}
+		}
+	}
+}
--- Assets/WorldGenerator/WorldGenerator.cs
+++ Assets/WorldGenerator/WorldGenerator.cs
@@ -10,7 +10,6 @@ using System.Linq;
 
 namespace WorldGenerator {
     public class WorldGenerator : MonoBehaviour {
-		private const int vertexLimit = 63000;
 
 		public Material material;
 		public AnimationCurve initialDistributionCurve;
@@ -32,12 +31,13 @@ namespace WorldGenerator {
             gameObj.name = "Island";
 
             int seed = 12335;
-            world = generateWorldGeometry(seed);
+            world = generateWorld(seed);
 
 			WorldGenElevation.createIsland(world, waterClip);
 			// WorldGenBiomes.separateTheLandFromTheWater(world, new PerlinIslandShape(seed, worldSize));
 
-			triangulate(world, gameObj);
+			WorldGenMesh meshGenerator = new WorldGenMesh(world, gameObj, verticalScale, material);
+			meshGenerator.triangulate();
         }
 
 		void OnDrawGizmos() {
@@ -77,7 +77,7 @@ namespace WorldGenerator {
 			}
 		}
 
-        private World generateWorldGeometry(int seed) {
+        private World generateWorld(int seed) {
 			VoronoiBase voronoi = WorldGeneratorUtils.generateVoronoi(seed, worldSize, pointCount, initialDistributionCurve);
 
 			List<Corner> corners = WorldGeneratorUtils.createCorners(voronoi.Vertices, worldSize);
@@ -90,133 +90,5 @@ namespace WorldGenerator {
 
 			return new World(seed, worldSize, centers, corners, edges);
         }
-
-		private void triangulate(World world, GameObject islandGameObject) {
-			List<int> indices = new List<int>();
-			List<Vector3> vertices = new List<Vector3>();
-			List<Color> colors = new List<Color>();
-			List<Center> sortedCenters = new List<Center>(world.centers);
-			sortedCenters.Sort((a, b) => {
-				bool xLess = a.coord.x < b.coord.x;
-				return xLess || (xLess && a.coord.y < b.coord.y) ? -1 : 1;
-			});
-			foreach (Center center in sortedCenters) {
-				addTrianglesForCenter(center, indices, vertices, colors);
-				if (vertices.Count > vertexLimit) {
-					addMeshSubObject(islandGameObject, indices, vertices, colors);
-				}
-			}
-			if (vertices.Count > 0) {
-				addMeshSubObject(islandGameObject, indices, vertices, colors);
-			}
-		}
-
-		private void buildPedestal(World world, GameObject islandGameObject) {
-			List<int> indices = new List<int>();
-			List<Vector3> vertices = new List<Vector3>();
-			List<Color> colors = new List<Color>();
-			foreach (Center center in world.centers) {
-				if (center.isOnRim) {
-					if (vertices.Count > vertexLimit) {
-						addMeshSubObject(islandGameObject, indices, vertices, colors);
-					}
-				}
-			}
-			if (vertices.Count > 0) {
-				addMeshSubObject(islandGameObject, indices, vertices, colors);
-			}
-		}
-
-        private void addMeshSubObject(GameObject containingObject, List<int> indices, List<Vector3> vertices, List<Color> colors) {
-			GameObject gameObject = new GameObject();
-			gameObject.name = "mesh section";
-			gameObject.transform.SetParent(containingObject.transform);
-			gameObject.transform.position = Vector3.zero;
-
-            MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
-            Mesh mesh = meshFilter.mesh;
-            gameObject.AddComponent<MeshRenderer>();
-            gameObject.AddComponent<MeshCollider>().sharedMesh = mesh;
-			gameObject.GetComponent<Renderer>().material = material;
-
-			mesh.vertices = vertices.ToArray();
-			mesh.triangles = indices.ToArray();
-			mesh.colors = colors.ToArray();
-			mesh.RecalculateNormals();
-			Vector2[] myUVs = new Vector2[vertices.Count];
-			for (var i = 0; i < vertices.Count; i++) {
-				myUVs[i] = new Vector2(vertices[i].x / worldSize, vertices[i].y / worldSize);
-			}
-			mesh.uv = myUVs;
-			
-			indices.Clear();
-			vertices.Clear();
-			colors.Clear();
-		}
-
-        private void addTrianglesForCenter(Center center, 
-											List<int> indices, 
-											List<Vector3> vertices,
-											List<Color> colors) {
-            Vector3 vertexCenter = new Vector3((float)center.coord.x, center.scaledElevation(verticalScale), (float)center.coord.y);
-            Color color = getColor(center.terrainType);
-			if (center.isClipped) {
-				return;
-			}
-
-            List<Corner> corners = center.corners;
-            for (int i = 0; i < corners.Count; i++) {
-				Corner corner1 = corners[i];
-				int index2 = i + 1 >= corners.Count ? 0 : i + 1;
-				Corner corner2 = corners[index2];
-
-				if (corner1.isClipped || corner2.isClipped) {
-					continue;
-				} else {
-					Vector3 vertex1 = new Vector3((float)corner1.coord.x, corner1.scaledElevation(verticalScale), (float)corner1.coord.y);
-					Vector3 vertex2 = new Vector3((float)corner2.coord.x, corner2.scaledElevation(verticalScale), (float)corner2.coord.y);
-					addTriangle(vertexCenter, vertex1, vertex2, color, indices, vertices, colors);
-				}
-            }
-		}
-
-		private static void addTriangle(Vector3 a, Vector3 b, Vector3 c, Color color, 
-									List<int> indices, List<Vector3> vertices, List<Color> colors) {
-				int indicesOffset = vertices.Count;
-            	vertices.Add(a);
-            	vertices.Add(b);
-            	vertices.Add(c);
-				colors.Add(color);
-				colors.Add(color);
-				colors.Add(color);
-                indices.Add(indicesOffset);
-                indices.Add(indicesOffset + 2);
-                indices.Add(indicesOffset + 1);
-		}
-
-        private void assignVertexColors(World world, Mesh mesh) {
-			Color[] colors = new Color[mesh.vertices.Length];
-			foreach (Corner corner in world.corners) {
-				foreach (int index in corner.vertexIndices) {
-					colors[index] = getColor(corner.terrainType);
-				}
-			}
-			mesh.colors = colors;
-		}
-
-		private Color getColor(TerrainType terrainType) {
-			switch (terrainType) {
-				case TerrainType.OCEAN:
-					return Color.blue;
-				case TerrainType.LAKE:
-					return Color.cyan;
-				case TerrainType.LAND:
-					return Color.grey;
-                case TerrainType.COAST:
-                    return Color.yellow;
-                default:
-					return Color.white;
-			}
-		}
     }
 }
