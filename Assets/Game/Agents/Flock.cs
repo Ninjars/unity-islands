f6afe713b9e55237990bd9c84f7c50159fd43136
Jeremy Stevens <jeremy.stevens@bglgroup.co.uk>
2018-05-05 15:09:03 +0100

    implemented flocks

---
+++ Assets/Game/Agents/Flock.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+	public class Flock : MonoBehaviour {
+		public GameObject flockAgentPrefab;
+		public int initialFlockSize = 10;
+		public int roamTimeMin = 10;
+		public int roamTimeMax = 20;
+		public bool drawDebug;
+
+		private float currentRoamTime;
+		private float currentRoamTimer;
+        private System.Random random;
+        private TerrainNode currentNode;
+		private List<FlockMember> members = new List<FlockMember>();
+
+
+        void Awake() {
+			if (gameObject.GetComponent<FlockMember>() == null) {
+				Debug.LogError("Flock agent must be a FlockMember");
+			}
+			random = WorldManager.instance.GetRandom();
+			currentNode = WorldManager.instance.getClosestTerrainNode(gameObject.transform.position);
+		}
+
+		void Start () {
+			for (int i = 0; i < initialFlockSize; i++) {
+				GameObject newMember = Instantiate(flockAgentPrefab, currentNode.getRandomPoint3D(), UnityEngine.Random.rotation);
+				FlockMember flockMember = newMember.GetComponent<FlockMember>();
+				flockMember.travelTo(currentNode);
+				members.Add(flockMember);
+			}
+		}
+		
+		void Update () {
+			currentRoamTime += Time.deltaTime;
+			if (currentRoamTime > currentRoamTimer) {
+				currentRoamTime = 0;
+				currentRoamTimer = roamTimeMin + (float)random.NextDouble() * roamTimeMax;
+
+				List<TerrainNode> neighbours = currentNode.neighbouringNodes;
+				int next = random.Next(neighbours.Count);
+				currentNode = neighbours[next];
+				gameObject.transform.position = currentNode.position;
+				foreach (FlockMember member in members) {
+					member.travelTo(currentNode);
+				}
+			}
+		}
+
+		private void OnDrawGizmos() {
+			if (!drawDebug) {
+				return;
+			}
+			Gizmos.color = Color.green;
+			foreach (FlockMember member in members) {
+				Gizmos.DrawLine(gameObject.transform.position, member.getPosition());
+			}
+		}
+	}
+
+	public interface FlockMember {
+		void travelTo(TerrainNode node);
+		Vector3 getPosition();
+		void regroup();
+	}
+}
---
+++ Assets/Game/Agents/FlockingAgent.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+	public class FlockingAgent : BaseAgent, FlockMember {
+        private System.Random random;
+        private TerrainNode currentNode;
+
+		private float currentDelayTime;
+		private float nextActionDelay;
+
+		public float minActionDelaySeconds = 5;
+		public float maxActionDelaySeconds = 10;
+		public bool debugDraw = false;
+
+		void Awake() {
+			base.init();
+			random = WorldManager.instance.GetRandom();
+		}
+
+		void Start () {
+			moveToRandomPoint();
+		}
+		
+		void Update () {
+			currentDelayTime += Time.deltaTime;
+			if (currentDelayTime > nextActionDelay) {
+				moveToRandomPoint();
+			}
+		}
+
+		private void moveToRandomPoint() {
+			if (currentNode == null) {
+				Debug.LogError("Flocking agent has no current node!");
+				return;
+			}
+			Vector3 targetPosition = currentNode.getRandomPoint3D();
+			currentDelayTime = 0;
+			nextActionDelay = minActionDelaySeconds + (float)random.NextDouble() * maxActionDelaySeconds;
+			MoveToLocation(targetPosition);
+		}
+
+		void OnDrawGizmos() {
+			if (debugDraw) {
+				base.debugNavigationDraw();
+			}
+		}
+
+        public void travelTo(TerrainNode node) {
+			currentNode = node;
+			moveToRandomPoint();
+        }
+
+        public Vector3 getPosition() {
+            return gameObject.transform.position;
+        }
+
+        public void regroup() {
+			Vector3 targetPosition = currentNode.getRandomPoint3D(0.25f);
+			currentDelayTime = 0;
+			nextActionDelay = maxActionDelaySeconds;
+			MoveToLocation(targetPosition);
+        }
+    }
+}
